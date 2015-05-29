d1fe18dc2c7f1f37f6a02749db21aae5c1e52e40
Jeff Hacker <reachjkh@yahoo.com>
2015-05-29 14:12:44 -0400

    removed .re import

--- mystery_word.py
+++ mystery_word.py
@@ -1,6 +1,14 @@
 # You can figure this out.
 
+# file input and random word
 
+import random
+
+with open('/usr/share/dict/words', ) as temp_file:
+    text_to_analyze = temp_file.read()
+    text_to_analyze = text_to_analyze.lower()
+#    list_to_analyze = text_to_analyze.split()
+print(text_to_analyze)
 
 
 
@@ -14,7 +22,7 @@
 # display: working through it
 word = ('cat')
 game_word = len(word) * "-"
-print(word)
+# print(word)
 display = list(game_word)
 display[0] = "c"
 display[2] = "t"
