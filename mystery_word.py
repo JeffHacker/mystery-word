f8f03e27fda32b5cea59b7881797556ac7bad032
Jeff Hacker <reachjkh@yahoo.com>
2015-05-29 13:34:07 -0400

    working on display logic

--- mystery_word.py
+++ mystery_word.py
@@ -1,1 +1,21 @@
 # You can figure this out.
+
+
+
+
+
+
+
+
+
+
+
+
+# display: working through it
+word = ('cat')
+game_word = len(word) * "-"
+print(word)
+display = list(game_word)
+display[0] = "c"
+display[2] = "t"
+print(display)
