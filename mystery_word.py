41ff86b079262fb4598bba6b67ad9dd34ebc3fa1
Jeff Hacker <reachjkh@yahoo.com>
2015-05-29 16:16:31 -0400

    working on user input

--- mystery_word.py
+++ mystery_word.py
@@ -11,7 +11,21 @@ with open('/usr/share/dict/words', ) as temp_file:
     text_to_analyze = text_to_analyze.lower().split()
     word = random.choice(text_to_analyze)
 print(word)
-# print(text_to_analyze)
+
+#start the game
+print("Would you like to play a game? \n")
+print("Guess the word by selecting one letter at a time. \n")
+print("Think carefully.  You only get 8 guesses \n")
+print("The mystery word contains {} letters.".format(len(word)))
+
+
+# input and check
+def letter_input(letter):
+    if len(letter) > 1:
+        guessed_letter = input("Please guess only one letter...")
+        return
+
+letter_input(letter = input("Guess a letter...")) # letter
 
 
 
@@ -19,10 +33,8 @@ print(word)
 
 
 
+# display
 
-
-# display: working through it
-# word = ('cat')  #this no longer needed due to file input logic
 game_word = len(word) * "-"
 # print(word)
 display = list(game_word)
