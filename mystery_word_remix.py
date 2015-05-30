434aa564bc16337df40de1f07c8695c9f3148ab3
Jeff Hacker <reachjkh@yahoo.com>
2015-05-30 18:57:52 -0400

    can populate a letter into the hidden word

--- mystery_word_remix.py
+++ mystery_word_remix.py
@@ -6,9 +6,7 @@ with open('/usr/share/dict/words', ) as temp_file:
     word = random.choice(text_to_analyze)
 print(word)
 game_word = len(word) * "-"
-word_list = list(game_word)
-print("word list {}".format(word_list))
-wrong_list = []
+word_list = game_word.split()
 
 # start the game
 print("Would you like to play a game? \n")
@@ -24,25 +22,5 @@ def letter_input(letter):
         return letter
     return letter  # PJ helped me walk through this function while I solved
 
-
-# check letter against secret word
-def check_letter(input_letter, word, word_list):
-    if input_letter in word:
-        counter = 0
-        for letter in word:
-            if input_letter == letter:  # Bekk
-                word_list[counter] = input_letter
-            counter += 1
-        return word_list
-
-
-input_letter = (letter_input(letter=input("Guess a letter...")))
+input_letter = list(letter_input(letter=input("Guess a letter...")))
 print(input_letter)
-
-# call function to populate the word_list for display
-word_list = check_letter(input_letter, word, word_list)
-print("word_list {}".format(word_list))
-
-
-# print(word)
-# print(' '.join(word_list))  # from Soren on the .join for nicer looking display
